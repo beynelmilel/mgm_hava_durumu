72398f286b0f8fd928f151d44a6681a93ef56ba6
A. Taha Baki <atahabaki@pm.me>
2022-07-29 20:27:42 +0300

    add license notifiers for data classes...

--- lib/models/hourly_forecast_response.dart
+++ lib/models/hourly_forecast_response.dart
@@ -1,3 +1,23 @@
+/*
+ *  MGM Weather Forecast App - A Flutter Weather Forecast Application.
+ *  Copyright (C) 2021 A. Taha Baki
+ *
+ *  This file is part of MGM Weather Forecast App.
+ *
+ *  MGM Weather Forecast App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MGM Weather Forecast App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MGM Weather Forecast App.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 class HourlyForecastResponse {
     HourlyForecastResponse({
         required this.tarih,
--- lib/models/hourly_response.dart
+++ lib/models/hourly_response.dart
@@ -1,5 +1,25 @@
 import 'package:mgm_hava_durumu/models/hourly_forecast_response.dart';
 
+/*
+ *  MGM Weather Forecast App - A Flutter Weather Forecast Application.
+ *  Copyright (C) 2021 A. Taha Baki
+ *
+ *  This file is part of MGM Weather Forecast App.
+ *
+ *  MGM Weather Forecast App is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MGM Weather Forecast App is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MGM Weather Forecast App.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /// Response of this URL
 /// `https://servis.mgm.gov.tr/web/tahminler/saatlik?istno={ISTNO}`
 /// is an Array of HourlyResponse...
